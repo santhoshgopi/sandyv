540a41ab5a032da92f03aa4df776f0112fcded06
santhoshgopi <aksgd99@gmail.com>
2016-06-07 14:35:44 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int s;
+printf("%d",S);
+if(s!=0)
+  if(s<0)
+    printf("negative");
+  else(s>0)
+    printf("positive");
+    
+else
+  printf("zero");
+}
