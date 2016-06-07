1be937fe675200e6774364a76967594dbdc783b8
santhoshgopi <aksgd99@gmail.com>
2016-06-07 16:14:51 +0530

    Create strvowel.py

---
+++ strvowel.py
@@ -0,0 +1,8 @@
+s=input("")
+
+d=len(s)
+count=0
+for i in range(0,d):
+    if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U'):
+        count+=1
+print(count)
